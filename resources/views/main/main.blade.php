f2c62ae27596814f0d53f8c7f306e8662fa6cb55
Matt Pierce <mpierce486@gmail.com>
2018-11-21 06:02:58 -0700

    Added content to main page

--- resources/views/main/main.blade.php
+++ resources/views/main/main.blade.php
@@ -39,24 +39,66 @@
 </div>
 
 <div id="main-bottom-wrapper">
-	<div id="main-bottom" class="container">
-		<div class="row">
-			<div id="main-bottom-left" class="col-sm-6">
-				<div id="left-graphic">
-					<i class="fas fa-users"></i>
+	<div id="main-bottom-1-wrapper">
+		<div id="main-bottom-1" class="container">
+			<div class="row">
+				<div id="main-bottom-left" class="col-sm-12 col-md-6">
+					<div id="left-graphic">
+						<i class="fas fa-users"></i>
+					</div>
+					<div id="left-text">
+						We are a network of financial advisors, estate planning attorneys, and certified public accountants that believe in 
+						working together to help you plan for your future.
+					</div>
 				</div>
-				<div id="left-text">
-					We are a network of financial advisors, estate planning attorneys, and certified public accountants that believe in 
-					working together to help you plan for your future.
+
+				<div id="main-bottom-right" class="col-sm-12 col-md-6">
+					<div id="left-graphic">
+						<i class="fas fa-lightbulb"></i>
+					</div>
+					<div id="right-text">
+						We believe each individual and family should have advice around their financial, estate, and tax needs and our mission is to make that advice more accessible.
+					</div>
 				</div>
 			</div>
-
-			<div id="main-bottom-right" class="col-sm-6">
-				<div id="left-graphic">
-					<i class="fas fa-lightbulb"></i>
+		</div>
+	</div>
+	<div id="main-bottom-2-wrapper">
+		<div id='main-bottom-2'>
+			<div id="main-bottom-2-header">How Does It Work?</div>
+			<div class="main-bottom-text-wrapper row">
+				<div class="main-bottom-text col-md-8 col-sm-12">
+					<div class="main-bottom-text-header">Ask A Question</div>
+					<p class="main-bottom-text-copy">Choose from any of the topics above. Answer a few details about your situation and submit your question.</p>
 				</div>
-				<div id="right-text">
-					We believe each individual and family should have advice around their financial, estate, and tax needs and our mission is to make that advice more accessible.
+				<div class="main-bottom-graphic col-md-4">
+					<i class="fas fa-question-circle"></i>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="main-bottom-3-wrapper">
+		<div id='main-bottom-3'>
+			<div class="main-bottom-text-wrapper row">
+				<div class="main-bottom-text col-md-8 col-sm-12">
+					<div class="main-bottom-text-header">Receive Reponses From Advisors</div>
+					<p class="main-bottom-text-copy">We let advisors know about your question and they will provide a response to you. We will notify you whenever you get a response.</p>
+				</div>
+				<div class="main-bottom-graphic col-md-4">
+					<i class="fas fa-reply"></i>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="main-bottom-4-wrapper">
+		<div id='main-bottom-4'>
+			<div class="main-bottom-text-wrapper row">
+				<div class="main-bottom-text col-md-8 col-sm-12">
+					<div class="main-bottom-text-header">Engage With Advisors</div>
+					<p class="main-bottom-text-copy">Once you receive some responses, if you like any of those reponses you can create a discussion with an advisor which is similar to a private chat with that advisor where you can get into more details to help with your question.</p>
+				</div>
+				<div class="main-bottom-graphic col-md-4">
+					<i class="fas fa-comments"></i>
 				</div>
 			</div>
 		</div>
--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -15,7 +15,8 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/solid.js" integrity="sha384-+Ga2s7YBbhOD6nie0DzrZpJes+b2K1xkpKxTFFcx59QmVPaSA8c7pycsNaFwUK6l" crossorigin="anonymous"></script>
-        <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/fontawesome.js" integrity="sha384-7ox8Q2yzO/uWircfojVuCQOZl+ZZBg2D2J5nkpLqzH1HY0C1dHlTKIbpRz/LG23c" crossorigin="anonymous"></script> 
+        <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/fontawesome.js" integrity="sha384-7ox8Q2yzO/uWircfojVuCQOZl+ZZBg2D2J5nkpLqzH1HY0C1dHlTKIbpRz/LG23c" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
         
 
         <!-- JavaScript -->
