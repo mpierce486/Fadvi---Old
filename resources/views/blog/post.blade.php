da1ee67db1c6ab9add28585a458f79ebf8f9ef2f
Matt Pierce <mpierce486@gmail.com>
2019-03-06 05:34:36 -0700

    Update Blog Styling

--- resources/views/blog/post.blade.php
+++ resources/views/blog/post.blade.php
@@ -4,8 +4,8 @@
 
 <div class="global-wrapper container">
 	<div id="blog-post-wrapper">
-		<div id="blog-post-img">
-			<img src="{{ $blog->blog_main_img }}" class="img-responsive" />
+		<div id="blog-post-img" class="col-sm">
+			<img src="{{ $blog->blog_main_img }}" class="img-fluid" />
 		</div>
 		<div id="blog-post-title"><h2>{{ $blog->blog_title }}</h2></div>
 		<div id="blog-post-date">{{ date("F j, Y", strtotime($blog->created_at)) }}</div>
