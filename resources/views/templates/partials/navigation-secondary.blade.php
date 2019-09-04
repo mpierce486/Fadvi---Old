dadaebbef2ec16d0d6861c83c2b542e67b282d60
Matt Pierce <mpierce486@gmail.com>
2019-09-04 05:24:54 -0700

    Update links

--- resources/views/templates/partials/navigation-secondary.blade.php
+++ resources/views/templates/partials/navigation-secondary.blade.php
@@ -6,7 +6,7 @@
 	<div class="collapse navbar-collapse" id="navbarToggler">
 		<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
 			<li class="nav-item">
-				<a class="nav-link" href="{{ route('advisors') }}">Our Advisors</a>
+				<!-- <a class="nav-link" href="#">Our Advisors</a> -->
 			</li>
 			<li class="nav-item">
 				<a class="nav-link" href="{{ route('blog') }}">Fadvi Learning Center</a>
